e24ef42a48c65b265c65d08df75917fa267abf56
githubmutt@gmail.com <github.mutt@gmail.com>
2023-07-05 20:50:57 -0700

    epochdate

--- client/src/library/library.js
+++ client/src/library/library.js
@@ -5,9 +5,25 @@ function epoch( ){
     return new Date().getTime()
 }
 
+function epochdate(_epoch){
+    let epoch = 0 + _epoch
+
+    let t = new Date(0)
+    t.setMilliseconds(epoch)
+
+    let month = t.toLocaleString('default', { month: 'long' });
+    let day = t.getUTCDate();
+    let year = t.getUTCFullYear();
+
+    console.log( month + " " + day + " " + year )
+
+    return month + " " + day + " " + year 
+
+}
+
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 
-export { epoch, random}
+export { epoch, epochdate, random}
