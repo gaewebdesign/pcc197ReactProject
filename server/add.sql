aecc7ee48043f712f53dad4621efab5fa8fab007
githubmutt@gmail.com <github.mutt@gmail.com>
2023-07-11 01:10:40 -0700

    add.sql

--- server/add.sql
+++ server/add.sql
@@ -1,11 +1,11 @@
-/*
+
 INSERT INTO `user` 
 VALUES
 (NULL,9001,1,'tom','Tom Brady','tommy','xman@go.pasadena.edu',  "818-431-2312", NULL,NULL,NULL,NULL),
 (NULL,9002,2,'roger','Roger Okamoto','roger','rokamoto@go.pasadena.edu',  "818-431-2312", NULL,NULL,NULL,NULL),
 (NULL,9003,3,'cecilia','Cecilia Won','cecilia','cici@gmail.com',  "818-431-2312", NULL,NULL,NULL,NULL)
 ;
-*/
+
 
 INSERT INTO `resourceid` 
 VALUES
