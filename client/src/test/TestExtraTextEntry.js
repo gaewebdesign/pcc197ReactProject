e665903edc8a3323d3f627cd69af160c191ca48d
githubmutt@gmail.com <github.mutt@gmail.com>
2023-07-22 02:17:45 -0700

    before adding bootstrap

--- client/src/test/TestExtraTextEntry.js
+++ client/src/test/TestExtraTextEntry.js
@@ -4,96 +4,120 @@
 Test adding another TextBox
 for Capabilities
 save to localStorage
+https://www.telerik.com/blogs/how-to-programmatically-add-input-fields-react-forms
+
+https://www.freecodecamp.org/news/build-dynamic-forms-in-react/
+
 
 */
 
-
-
 import * as CONSTANTS from '../pages/Constants.js'
 import '../pages/Page.css'
-import {useState} from 'react'
+import {useState,useRef} from 'react'
 //    localStorage.getItem("loggedin")
 //    localStorage.setItem("password", info.password )
 
+function InputBox(){
+    const [ inputFields, setInputFields ]  = useState( [
+        {name:'Janet' } ,
+        {name:'Rae' }        
 
-const TestExtraTexEntry = () => {
-    const [ text, setText] = useState()
+    ] )
 
-const ClearButton = (evt)=> {
-
-    sessionStorage.removeItem("cap")
-    
-    alert( sessionStorage.getItem("cap") )
-  
-  
-}
-
-
-const AddButton= (evt)=>{
-
-    let v = sessionStorage.getItem("cap")
-    if( v == undefined){
-
-      sessionStorage.setItem("cap",text)
-      alert("initial: " + sessionStorage.getItem("cap"))
-
-
-    }else{
-        let s =  sessionStorage.getItem("cap")
-
-        sessionStorage.setItem("cap",s + "," + text)
-
-        alert("found " + sessionStorage.getItem("cap"))
+    const addFields = (evt) => {
+       evt.preventDefault()
+       let newField = {name: 'Erin'}
+       setInputFields( [ ...inputFields , newField])
 
     }
 
+    const clearFields = (evt) => {
+         evt.preventDefault()
+         setInputFields( [{name: ''}])
+    }
+    
+
+    const handleFormChange = (index, event) => {
+        let data = [...inputFields];
+        data[index][event.target.name] = event.target.value;
+
+        sessionStorage.setItem("cap", JSON.stringify(data) )
+        console.log( data )
+
+        try{
+             let obj = JSON.parse(sessionStorage.getItem('cap'))
+             console.log("****   *****")
+             console.log(obj )
+             console.log("****   *****")
+        }catch{
+
+             console.log("ERR")
+
+        }
+        setInputFields( data )
+
+    }
+    return(
+        <div className="InputBox">
+        
+{/*         <form onSubmit={addFields}> */}
+
+         <h1> InputBoxes</h1>
+         {
+
+            inputFields?.map(
+                (input, index) => {
+                   return(
+                    <div key={index}>
+                    <input
+                      name='name'
+                      onChange={event => handleFormChange(index, event)}
+                    />
+                 </div>
+
+           )
+
+                   
+
+         })
+
+        }
+
+        <button type="button"
+        class="btn btn-secondary"
+            onClick={clearFields}
+        >
+        Clear
+        </button>
+        &nbsp;
+          <button type="button"
+          class="btn btn-primary"
+              onClick={addFields}
+          >
+          Add
+          </button>
+          &nbsp;
+ 
+        
+  {/*       </form>  */}
+        
+         
+        </div>
+
+
+    )
+
+
 }
 
-const onChange = (evt) => {
-       
-       setText(evt.target.value)
-}
+
+const TestExtraTextEntry = () => {
 
    
   return (
-
             <div>
-
-             <div class="form-group">
-  
-             </div>
-                 <div class="form-group">
-                 <label >Cap:</label>
-                 <input 
-                     id= "_id"
-                     type="text"
-                     onChange = {onChange}
-                 ></input>
-                 </div>
-             
-                 <hr/>
-                 {text}<br/>
-                 <button
-                       type="button"
-                       class="btn btn-primary"
-                       onClick= {AddButton }
-                     ><b>Add </b>
-                 </button>
-                 &nbsp;
-  
-                 <p/>
-
-                 <button
-                 type="button"
-                 class="btn btn-primary"
-                 onClick= {ClearButton }
-               ><b>Clear </b>
-           </button>
-  
-
-
-                 <p/>
-   
+            <h1>TextBox </h1>
+              <InputBox/>
             </div>
 
 
@@ -103,4 +127,4 @@ const onChange = (evt) => {
 }
 
 
-export {TestExtraTexEntry}
+export {TestExtraTextEntry}
